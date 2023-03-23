3038d595918eae4c8e5e034b818b19870766de44
Nikita60012 <96587581+Nikita60012@users.noreply.github.com>
2023-03-23 11:14:03 +0300

    Add files via upload

---
+++ Lab_11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    internal class Program
+    {
+        
+        enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        
+
+        public static void Main(string[] args)
+        {
+            int count = 0;
+
+            Day currentDay = (ref int counter) =>
+            {
+                if (count == 7)
+                    counter = 0;
+
+                DayOfWeek day = () => Console.WriteLine(DaysOfWeek.GetValues(typeof(DaysOfWeek)).GetValue(count));
+                day();
+                count++;
+            };
+            currentDay(ref count);
+            currentDay(ref count);
+            currentDay(ref count);
+            currentDay(ref count);
+            currentDay(ref count);
+            currentDay(ref count);
+            currentDay(ref count);
+            currentDay(ref count);
+
+
+        }
+        delegate void DayOfWeek();
+        delegate void Day(ref int count);
+    }
+}
